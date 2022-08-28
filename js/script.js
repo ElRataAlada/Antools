77211bd633a10d73664d9f546a3e12973e9b9000
ElRataAlada <110470848+ElRataAlada@users.noreply.github.com>
2022-08-28 22:04:49 +0300

    Version 0.1

---
+++ js/script.js
@@ -0,0 +1,141 @@
+<<<<<<< HEAD
+const actions = document.querySelectorAll(".action_container")
+
+actions.forEach((action) => {
+    action.addEventListener('click', (e)=>{
+        e.currentTarget.classList.toggle('active')
+    })
+})
+
+
+const cards = document.querySelectorAll('#card')
+
+cards.forEach((card) => card.addEventListener('mouseenter', (e) => {
+    e.target.classList.add('active')
+    e.target.querySelector('.btn').classList.add('active')
+}))
+
+cards.forEach((card) => card.addEventListener('mouseleave', (e) => {
+    e.target.classList.remove('active')
+    e.target.querySelector('.btn').classList.remove('active')
+}))
+
+
+
+const anims = document.querySelectorAll('.animated')
+const layer = document.querySelector('.layer')
+
+window.addEventListener('scroll', animateOnScroll)
+
+animateOnScroll()
+
+function animateOnScroll(){
+    for (let i = 0; i < anims.length; i++) {
+        const item = anims[i];
+
+        let animationPoint = scrollY + window.innerHeight - window.innerHeight/8;
+
+        (scrollY >= window.innerHeight) ?  layer.classList.add('fixed') : layer.classList.remove('fixed');
+
+        (animationPoint >= item.offsetTop) ? item.classList.add('_active') : false;
+    }
+}
+
+function getOffset(item){
+    return item.getBoundingClientRect().top;
+}
+
+
+const burger = document.querySelector('.burger')
+const popup= document.querySelector('.popup')
+const nav = document.querySelector('.header__nav').cloneNode(1)
+
+burger.addEventListener('click', (e)=>{
+    burger.classList.toggle('active')
+    popup.appendChild(nav)
+    document.body.classList.toggle('fixed')
+    popup.classList.toggle('open')
+})
+
+nav.querySelectorAll('#link').forEach((link) => {
+    link.addEventListener('click', (e)=>{
+        e.preventDefault()
+        burger.classList.toggle('active')
+        popup.removeChild(nav)
+        document.body.classList.toggle('fixed')
+        popup.classList.toggle('open')
+    })
+})
+
+
+=======
+const actions = document.querySelectorAll(".action_container")
+
+actions.forEach((action) => {
+    action.addEventListener('click', (e)=>{
+        e.currentTarget.classList.toggle('active')
+    })
+})
+
+
+const cards = document.querySelectorAll('#card')
+
+cards.forEach((card) => card.addEventListener('mouseenter', (e) => {
+    e.target.classList.add('active')
+    e.target.querySelector('.btn').classList.add('active')
+}))
+
+cards.forEach((card) => card.addEventListener('mouseleave', (e) => {
+    e.target.classList.remove('active')
+    e.target.querySelector('.btn').classList.remove('active')
+}))
+
+
+
+const anims = document.querySelectorAll('.animated')
+const layer = document.querySelector('.layer')
+
+window.addEventListener('scroll', animateOnScroll)
+
+animateOnScroll()
+
+function animateOnScroll(){
+    for (let i = 0; i < anims.length; i++) {
+        const item = anims[i];
+
+        let animationPoint = scrollY + window.innerHeight - window.innerHeight/8;
+
+        (scrollY >= window.innerHeight) ?  layer.classList.add('fixed') : layer.classList.remove('fixed');
+
+        (animationPoint >= item.offsetTop) ? item.classList.add('_active') : false;
+    }
+}
+
+function getOffset(item){
+    return item.getBoundingClientRect().top;
+}
+
+
+const burger = document.querySelector('.burger')
+const popup= document.querySelector('.popup')
+const nav = document.querySelector('.header__nav').cloneNode(1)
+
+burger.addEventListener('click', (e)=>{
+    burger.classList.toggle('active')
+    popup.appendChild(nav)
+    document.body.classList.toggle('fixed')
+    popup.classList.toggle('open')
+})
+
+nav.querySelectorAll('#link').forEach((link) => {
+    link.addEventListener('click', (e)=>{
+        e.preventDefault()
+        burger.classList.toggle('active')
+        popup.removeChild(nav)
+        document.body.classList.toggle('fixed')
+        popup.classList.toggle('open')
+    })
+})
+
+
+>>>>>>> 4220f7276909586968ae3cf2d2d6737117a9e765
---
+++ js/slider.js
@@ -0,0 +1,239 @@
+<<<<<<< HEAD
+let position = 0
+
+const slider = document.querySelector('.slider__wrapper')
+const track = document.querySelector('.slider_track')
+const items = document.querySelectorAll('.slider_item')
+
+const prev = slider.querySelector('.prev')
+const next = slider.querySelector('.next')
+
+const indicators = document.querySelectorAll('.slider_indicator')
+
+const itemWidth = slider.clientWidth
+const movePosition = itemWidth
+
+items.forEach((item) => {
+    item.style.cssText = `min-width: ${itemWidth};`
+})
+
+moveSlider()
+
+next.addEventListener('click', (e) => {
+    removeItem()
+
+    position++
+    
+    moveSlider()
+})
+
+prev.addEventListener('click', (e) => {
+    removeItem()
+    
+    position--
+    
+    moveSlider()
+})
+
+function moveSlider(){
+    if(position > items.length-1) {position = 0}
+    else if(position < 0) {position = items.length-1}
+
+    indicators[position].classList.add('active')
+    items[position].classList.add('active')
+    
+    track.style.cssText = `transform: translateX(${-position * movePosition}px);`
+}
+
+function removeItem(){
+    items[position].classList.remove('active')
+    indicators[position].classList.remove('active')
+}
+
+indicators.forEach((indicator, index) => {
+    indicator.addEventListener('click', (e) => {
+        removeItem()
+
+        position = index
+
+        moveSlider()
+    })
+})
+
+let startX
+let pressed
+
+const moveOffset = 100
+
+track.addEventListener('mousedown', (e) => {
+    startX = e.offsetX
+    pressed = true
+})
+
+track.addEventListener('mouseup', () => {
+    pressed = false
+})
+
+track.addEventListener('mousemove', (e) => {
+    if(!pressed) {return}
+
+    const mouseMove =  startX - e.offsetX
+
+    if(mouseMove > moveOffset) {
+        removeItem(); 
+
+        position++;
+
+        if(position > items.length-1) {position = items.length-1}
+        
+        moveSlider(); 
+        pressed = false;
+    }
+    else if(mouseMove < -moveOffset) {
+        removeItem(); 
+        position--;
+
+        if(position < 0) {position = 0}
+
+        moveSlider(); 
+        pressed = false;
+    }
+})
+
+let hovered = false
+
+slider.addEventListener('mouseenter', ()=>{
+    hovered = true
+})
+
+slider.addEventListener('mouseout', ()=>{
+    hovered = false
+})
+
+setInterval(()=>{
+    if(hovered){return false}
+
+    removeItem();
+    position++;
+    moveSlider();
+}, 10000)
+=======
+let position = 0
+
+const slider = document.querySelector('.slider__wrapper')
+const track = document.querySelector('.slider_track')
+const items = document.querySelectorAll('.slider_item')
+
+const prev = slider.querySelector('.prev')
+const next = slider.querySelector('.next')
+
+const indicators = document.querySelectorAll('.slider_indicator')
+
+const itemWidth = slider.clientWidth
+const movePosition = itemWidth
+
+items.forEach((item) => {
+    item.style.cssText = `min-width: ${itemWidth};`
+})
+
+moveSlider()
+
+next.addEventListener('click', (e) => {
+    removeItem()
+
+    position++
+    
+    moveSlider()
+})
+
+prev.addEventListener('click', (e) => {
+    removeItem()
+    
+    position--
+    
+    moveSlider()
+})
+
+function moveSlider(){
+    if(position > items.length-1) {position = 0}
+    else if(position < 0) {position = items.length-1}
+
+    indicators[position].classList.add('active')
+    items[position].classList.add('active')
+    
+    track.style.cssText = `transform: translateX(${-position * movePosition}px);`
+}
+
+function removeItem(){
+    items[position].classList.remove('active')
+    indicators[position].classList.remove('active')
+}
+
+indicators.forEach((indicator, index) => {
+    indicator.addEventListener('click', (e) => {
+        removeItem()
+
+        position = index
+
+        moveSlider()
+    })
+})
+
+let startX
+let pressed
+
+const moveOffset = 100
+
+track.addEventListener('mousedown', (e) => {
+    startX = e.offsetX
+    pressed = true
+})
+
+track.addEventListener('mouseup', () => {
+    pressed = false
+})
+
+track.addEventListener('mousemove', (e) => {
+    if(!pressed) {return}
+
+    const mouseMove =  startX - e.offsetX
+
+    if(mouseMove > moveOffset) {
+        removeItem(); 
+
+        position++;
+
+        if(position > items.length-1) {position = items.length-1}
+        
+        moveSlider(); 
+        pressed = false;
+    }
+    else if(mouseMove < -moveOffset) {
+        removeItem(); 
+        position--;
+
+        if(position < 0) {position = 0}
+
+        moveSlider(); 
+        pressed = false;
+    }
+})
+
+let hovered = false
+
+slider.addEventListener('mouseenter', ()=>{
+    hovered = true
+})
+
+slider.addEventListener('mouseout', ()=>{
+    hovered = false
+})
+
+setInterval(()=>{
+    if(hovered){return false}
+
+    removeItem();
+    position++;
+    moveSlider();
+}, 10000)
+>>>>>>> 4220f7276909586968ae3cf2d2d6737117a9e765
